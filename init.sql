6965d869f9cc983e1f805e25fdcf779430134008
petrovaana <143181789+petrovaana@users.noreply.github.com>
2025-04-21 13:55:50 +0300

    Delete init.sql

--- init.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM classes;
-
-INSERT INTO classes (title, value) VALUES ("Tila", "Satama");
-INSERT INTO classes (title, value) VALUES ("Tila", "Helsinki");
-INSERT INTO classes (title, value) VALUES ("Tila", "Kanava");
-INSERT INTO classes (title, value) VALUES ("Tila", "Talviterassi");
-INSERT INTO classes (title, value) VALUES ("Tila", "Syysterassi");
-INSERT INTO classes (title, value) VALUES ("Tila", "Sisäpiha");
-INSERT INTO classes (title, value) VALUES ("Tila", "Grilli");
