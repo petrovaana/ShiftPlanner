b0b3d4920628029bc104cc19243236082ddb25a4
petrovaana <143181789+petrovaana@users.noreply.github.com>
2025-04-21 14:01:20 +0300

    Update items.py
    New clean version

--- items.py
+++ items.py
@@ -1,98 +1,81 @@
 import db
+from datetime import datetime
 
-def get_all_classes():
-    sql = "SELECT title, value FROM classes ORDER BY id"
-    result = db.query(sql)
+def add_item(title, description, booked_space, guests, payment, start_price, date, user_id):
+    sql = "INSERT INTO items (title, description, booked_space, guests, payment, start_price, date, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+    db.execute(sql, [title, description, booked_space, guests, payment, start_price, date, user_id])
 
-    classes = {}
-    for title, value in result:
-        classes[title] = []
-    for title, value in result:
-        classes[title].append(value)
-    
-    return classes
-
-def add_item(title, description, pax, maksutapa, start_price, pvm, user_id, classes):
-    all_classes = get_all_classes()
-
-    sql = "INSERT INTO items (title, description, pax, maksutapa, start_price, pvm, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)"
-    db.execute(sql, [title, description, pax, maksutapa, start_price, pvm, user_id])
-
-    item_id = db.last_insert_id()
-
-    sql = "INSERT INTO item_classes (item_id, title, value) VALUES (?, ?, ?)"
-    for title, value in classes:
-        db.execute(sql, [item_id, title, value])
-
-
-#Muokkauksiin just et kuka muokannu viimesimmän kerran liittyvät tiedot
-def add_tiedot(item_id, user_id, description):
-    sql = "INSERT INTO muokkaukset (item_id, user_id, description) VALUES (?, ?, ?)"
+def add_information(item_id, user_id, description):
+    sql = "INSERT INTO edits (item_id, user_id, description) VALUES (?, ?, ?)"
     db.execute(sql, [item_id, user_id, description])
 
-def get_tiedot(item_id):
-    sql = """SELECT muokkaukset.description, users.username
-            FROM muokkaukset, users
-            WHERE muokkaukset.item_id = ? AND muokkaukset.user_id = users.id
-            ORDER BY muokkaukset.id DESC"""
+def get_information(item_id):
+    sql = """SELECT edits.description, users.id user_id, users.username
+            FROM edits, users
+            WHERE edits.item_id = ? AND edits.user_id = users.id
+            ORDER BY edits.id"""
     return db.query(sql, [item_id])
-
-def get_classes(item_id):
-    sql = "SELECT title, value FROM item_classes WHERE item_id = ?"
-    return db.query(sql, [item_id])
-
 
 def get_items():
-    sql = "SELECT id, title, tila, pvm FROM items ORDER BY DESC"
+    sql = "SELECT id, title, date FROM items ORDER BY date DESC"
     return db.query(sql)
 
+def get_items_indexpage():
+    sql = """SELECT id, title, date 
+            FROM items 
+            ORDER BY CASE
+                WHEN date = ? THEN 0
+                ELSE 1
+            END, date ASC
+            LIMIT 7"""
+    today = datetime.today().date()
+    return db.query(sql, [today])
+
 def get_item(item_id):
-    sql = """SELECT items.title, 
-                    items.id,
+    sql = """SELECT items.id, 
+                    items.title,
                     items.description,
-                    items.tila,
-                    items.pax,
-                    items.maksutapa,
+                    items.date,
+                    items.payment,
+                    items.booked_space,
                     items.start_price,
+                    items.guests,
                     users.id user_id,
                     users.username 
             FROM items, users
             WHERE items.user_id = users.id 
             AND items.id = ?"""
     result = db.query(sql, [item_id])
-    return result[0] if result else None
+    if result:
+        return result[0]
+    else:
+        return None
 
-
-def update_item(item_id, title, description, pax, maksutapa, start_price, pvm, classes):
+def update_item(item_id, title, description, booked_space, guests, payment, start_price, date):
     sql = """UPDATE items SET title = ?,
                             description = ?,
-                            pax = ?,
-                            maksutapa = ?,
+                            booked_space = ?,
+                            guests = ?,
+                            payment = ?,
                             start_price = ?,
-                            pvm = ?
+                            date = ?
                         WHERE id = ?"""
-    db.execute(sql, [title, description, pax, maksutapa, start_price, pvm, item_id])
+    db.execute(sql, [title, description, booked_space, guests, payment, start_price, date, item_id])
 
-    sql = "DELETE FROM item_classes WHERE item_id = ?"
-    db.execute(sql, [item_id])
-
-    sql = "INSERT INTO item_classes (item_id, title, value) VALUES (?, ?, ?)"
-    for title, value in classes:
-        db.execute(sql, [item_id, title, value])
-
-#Poistaa yksärin
 def remove_item(item_id):
-    sql = "DELETE FROM item_classes WHERE item_id = ?"
+    sql = "DELETE FROM edits WHERE item_id = ?"
     db.execute(sql, [item_id])
 
     sql = "DELETE FROM items WHERE id = ?"
     db.execute(sql, [item_id])
 
-#Pystyy ettii yksärii nimen, description tai tilan perusteella
 def find_items(query):
-    sql = """SELECT id, title
+    sql = """SELECT id, title, date
             FROM items
-            WHERE title LIKE ? OR description LIKE ? OR tila LIKE ?
+            WHERE LOWER(title) LIKE LOWER(?) 
+                OR LOWER(description) LIKE LOWER(?) 
+                OR LOWER(booked_space) LIKE LOWER(?)
             ORDER BY id DESC"""
     like = "%" + query + "%"
-    return db.execute(sql, [like, like])
+    results = db.query(sql, [like, like, like])
+    return results
