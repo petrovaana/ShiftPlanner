4bc2cb396b160db8e428c3928f7620e0ee9223fe
petrovaana <143181789+petrovaana@users.noreply.github.com>
2025-04-23 10:14:20 +0300

    Created init.sql file for classes
    Created init.sql file for classes.

---
+++ init.sql
@@ -0,0 +1,9 @@
+DELETE FROM classes;
+
+INSERT INTO classes (title, value) VALUES ('Reason', 'rotten');
+INSERT INTO classes (title, value) VALUES ('Reason', 'wrong item');
+INSERT INTO classes (title, value) VALUES ('Reason', 'employee meal');
+INSERT INTO classes (title, value) VALUES ('Reason', 'accident');
+
+INSERT INTO classes (title, value) VALUES ('Where', 'floor');
+INSERT INTO classes (title, value) VALUES ('Where', 'kitchen');
