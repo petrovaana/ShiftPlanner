fce8f1f0068c54018a1bb96e7d87a78d49aefa1d
petrovaana <143181789+petrovaana@users.noreply.github.com>
2025-04-23 10:30:53 +0300

    Added users.py
    Added a file. Accidentally deleted it earlier.

---
+++ users.py
@@ -0,0 +1,37 @@
+import db
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def get_user(user_id):
+    sql = "SELECT id, username FROM users WHERE id = ?"
+    result = db.query(sql, [user_id])
+    return result[0] if result else None
+
+def get_items(user_id):
+    sql = "SELECT id, title FROM items WHERE user_id = ? ORDER BY id DESC"
+    return db.query(sql, [user_id])
+
+def create_user(username, password):
+    password_hash = generate_password_hash(password)
+    sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+    db.execute(sql, [username, password_hash])
+
+def check_login(username, password):
+    sql = "SELECT id, password_hash FROM users WHERE username = ?"
+    result = db.query(sql, [username])
+    if not result:
+        return None
+    user_id = result[0]["id"]
+    password_hash = result[0]["password_hash"]
+    if check_password_hash(password_hash, password):
+        return user_id
+    else:
+        return None
+
+def update_image(user_id, image):
+    sql = "UPDATE users SET image = ? WHERE id = ?"
+    db.execute(sql, [image, user_id])
+
+def get_image(user_id):
+    sql = "SELECT image FROM users WHERE id = ?"
+    result = db.query(sql, [user_id])
+    return result[0][0] if result else None
